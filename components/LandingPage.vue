a0df646e481399fd06da6999ddf0763d491d6213
Ananya Srinivasan <ananya.srinivasan@gmail.com>
2026-02-02 19:04:00 -0500

    redid landing page with all elements as background

--- components/LandingPage.vue
+++ components/LandingPage.vue
@@ -1,14 +1,6 @@
 <template>
     <div class="gradient">
-        <img src="@/assets/img/images/mountains_new.svg" class="mountains" />
-        <img src="@/assets/img/images/basic marshie (arms down).svg" class="marshie1" />
-        <img src="@/assets/img/images/basic marshie (arms up).svg" class="marshie2" />
-        <div class="ufo-group">
-            <img src="@/assets/img/images/ufo.svg" class="ufo" />
-            <img src="@/assets/img/images/ufo rays.svg" class="ufo-rays" />
-        </div>
-        <img src="@/assets/img/images/text border.svg" class="text-border" />
-        <img src="@/assets/img/images/title_and_tagline.svg" class="name" />
+        <img src="../assets/img/images/landing_background.webp" class="background" alt="landing image" />
         <div class="content-wrapper">
             <div class="button-wrapper">
                 <a target="_blank" href="https://register.bit.camp" class="register-button">REGISTER</a>
@@ -33,67 +25,25 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
-    width: 100vw;
+    min-height: 75vw;
+    height: auto;
     overflow: hidden;
+    background-color: #0D2539;
     background-size: cover;
-    background-image: url("../assets/img/images/landing_gradient.webp");
-    margin-bottom: 150px; /* can be removed, only so that tracks doesn't overlap */
+    width: 100%;
+    aspect-ratio: 16 / 9;
 }
 
-
-.mountains {
+.background {
     position: absolute;
-    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    top: 0;
     left: 0;
     width: 100%;
-    z-index: 3;
-}
-
-.ufo {
-    position: absolute;
-    top: 23vh;
-    left: 22vw;
-    z-index: 2;
-    scale: 1.2;
-}
-
-.ufo-rays {
-    position: absolute;
-    top: 37vh;
-    left: 25vw;
-    z-index: 1;
-    scale: 1.2;
-}
-
-.marshie1 {
-    position: absolute;
-    top: 50vh;
-    left: 28vw;
-    z-index: 2;
-    scale: 1.2;
-}
-
-.marshie2 {
-    position: absolute;
-    top: 67vh;
-    left: 31vw;
-    z-index: 2;
-    scale: 1.2;
-}
-
-.text-border {
-    position: absolute;
-    left: 36vw;
-    top: 10vh;
-    scale: 1.2;
-}
-
-.name {
-    position: absolute;
-    left: 50vw;
-    top: 35vh;
-    scale: 1.2;
+    height: auto;
+    z-index: 0;
+    object-fit: cover;
 }
 
 .content-wrapper {
@@ -104,106 +54,63 @@ export default {
     justify-content: center;
     text-align: center;
     color: white;
-    z-index: 3;
-    right: 37vw;
-    top: 55vh;
-    scale: 0.7;
+    z-index: 1;
+
+    position: absolute;
+    top: 51%;
+    right: 38%;
+    transform: translateY(-60%);
+    width: auto;
+}
+
+.button-wrapper {
+    width: 100%;
+    padding: 0 0 3vw 3.2vw;
 }
 
 .register-button {
     background-color: #FF6F3F;
-    border: solid;
+    border: none;
+    border-radius: 5rem;
+    border-style: solid;
     border-color: #E54D1A;
-    border-width: 0.5rem;
-    border-radius: 20rem;
     color: white;
     font-family: Aleo, sans-serif;
-    font-size: 4rem;
     font-weight: bold;
-    padding: 1.7rem 4.8rem;
     letter-spacing: 0.2rem;
     margin-bottom: 10%;
     cursor: pointer;
     transition: transform 0.2s, box-shadow 0.2s;
+
+    font-size: clamp(1.3rem, 1.8vw, 2.8rem);
+    padding: clamp(0.4rem, 0.8vw, 1.1rem)
+             clamp(1.3rem, 2.5vw, 3.5rem);
+    border-width: clamp(0.2rem, 0.25vw, 0.4rem);
 }
 
-.register-button:hover {
-    transform: translateY(-4px);
-    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1);
-}
-
-@media (max-width: 796px) {
+@media (max-width: 402px) {
+    .background {
+        content: url("../assets/img/images/landing_mobile.webp");
+    }
     .register-button {
-        background-color: #FF6F3F;
-        border: solid;
-        border-color: #E54D1A;
-        border-radius: 20rem;
-        letter-spacing: 0.4rem;
-        border-width: 0.5rem;
+        border-radius: 5rem;
+        letter-spacing: 0.25rem;
+        font-size: clamp(1.1rem, 4vw, 1.9rem);
+        padding: clamp(0.6rem, 3vw, 1.2rem)
+             clamp(1.6rem, 6vw, 1.7rem);
+        border-width: clamp(0.18rem, 0.4vw, 0.2rem);
     }
 
     .content-wrapper {
-        top: 35vh;
-        left: -10vw;
-        scale: 0.6;
-    }
-
-    .text-border {
-        position: absolute;
-        left: -30vw;
-        top: -13vh;
-        scale: 0.8;
-    }
-
-    .name {
-        position: absolute;
-        left: 2vw;
-        top: 15vh;
-        scale: 0.8;
-    }
-
-    .ufo {
-        position: absolute;
-        top: 40vh;
-        left: 19vw;
-        z-index: 2;
-        scale: 0.9;
-    }
-
-    .ufo-rays {
-        position: absolute;
-        top: 47vh;
-        left: 25vw;
-        z-index: 1;
-        scale: 0.9;
-    }
-
-    .marshie1 {
-        position: absolute;
-        top: 63vh;
-        left: 37vw;
-        z-index: 2;
-        scale: 0.9;
-    }
-
-    .marshie2 {
-        position: absolute;
-        top: 78vh;
-        left: 45vw;
-        z-index: 2;
-        scale: 0.9;
-    }
-}
-
-/* @media (max-width: 500px) {
-    .content-wrapper {
-        width: 100%;
-        margin-top: calc(298% - 232.5vw);
+        top: 21%;
+        right: 54%;
+        transform: translateY(-60%);
     }
 
     .gradient {
-        background-color: #C4B500;
-        padding-bottom: calc(341% - 240vw);
+        background-color: #0D2539;
+        padding-bottom: calc(200%);
+        aspect-ratio: 9 / 16;
     }
-} */
+}
 </style>
